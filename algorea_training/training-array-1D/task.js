f019cee74cd59c75b41c18e2fd2128dd02600db2
mathias-hiron <mathias.hiron@gmail.com>
2017-04-24 14:57:38 +0200

    commit trainig-array-1D scratch

--- algorea_training/training-array-1D/task.js
+++ algorea_training/training-array-1D/task.js
@@ -33,7 +33,7 @@ function initTask(subTask) {
             includeAll: false,
             wholeCategories: ["variables"],
             singleBlocks: {
-               easy: ["lists_create_with_empty", "lists_getIndex", "lists_setIndex", "controls_repeat_ext", "controls_if"],
+               easy: ["lists_repeat", "lists_getIndex", "lists_setIndex", "controls_repeat_ext", "controls_if", "logic_compare", "math_number", "logic_boolean"],
                medium: ["lists_create_with_empty", "lists_getIndex", "lists_setIndex", "controls_repeat_ext", "controls_if", "math_number", "math_arithmetic", "logic_compare"],
                hard: ["lists_repeat", "lists_create_with_empty", "lists_getIndex", "lists_setIndex", "controls_repeat_ext", "controls_if", "math_number", "math_arithmetic", "logic_compare"]
             }
