59dbe5998964ed519e312cca93e16b67ab828d90
mathias-hiron <mathias.hiron@gmail.com>
2017-08-10 09:47:21 +0200

    remove unnecessary attributes, simplify description

--- algorea_training/training-array-1D/task.js
+++ algorea_training/training-array-1D/task.js
@@ -7,10 +7,10 @@ function initTask(subTask) {
       showLabels: true,
       maxIterWithoutAction: 2000,
       itemTypes: {
-         green_robot: { img: "green_robot.png", side: 80, nbStates: 9, isObstacle: true, offsetX: -14, category: "robot", team: 0, zOrder: 2 },
-         marker: { num: 2, img: "marker.png", side: cellSide, category: "marker", isObstacle: false, isMarker: true, hasColor: true, color: "marker", zOrder: 0 },
-         initialPaint: { num: 3, img: "paint.png", side: cellSide, category: "paint", isPaint: true, isMarker: true, color: "gris", zOrder: 1 },
-         paint: { img: "paint.png", side: cellSide, category: "paint", isPaint: true, color: "gris", zOrder: 1 }
+         green_robot: { img: "green_robot.png", side: 80, nbStates: 9, offsetX: -14, zOrder: 2 },
+         marker: { num: 2, img: "marker.png", side: cellSide, isMarker: true, zOrder: 0 },
+         initialPaint: { num: 3, img: "paint.png", side: cellSide, isPaint: true, isMarker: true, zOrder: 1 },
+         paint: { img: "paint.png", side: cellSide, isPaint: true, zOrder: 1 }
       },
       maxInstructions: {
          easy: 40,
@@ -24,19 +24,16 @@ function initTask(subTask) {
             hard: true
          },
          generatedBlocks: {
-            robot: {
-               easy: ["south", "east", "west", "paint", "paintOnCell", "col"],
-               medium: ["south", "east", "west", "paint", "paintOnCell", "col"],
-               hard: ["south", "east", "west", "paint", "paintOnCell", "col"]
-            }
+            robot: ["south", "east", "west", "paint", "paintOnCell", "col"],
          },
          standardBlocks: {
             includeAll: false,
             wholeCategories: ["variables"],
             singleBlocks: {
-               easy: ["lists_repeat", "lists_getIndex", "lists_setIndex", "controls_repeat_ext", "controls_if", "math_number", "logic_compare", "logic_boolean"],
-               medium: ["lists_repeat", "lists_getIndex", "lists_setIndex", "controls_repeat_ext", "controls_if", "math_number", "math_arithmetic", "logic_compare"],
-               hard: ["lists_repeat", "lists_create_with_empty", "lists_getIndex", "lists_setIndex", "controls_repeat_ext", "controls_if", "math_number", "math_arithmetic", "logic_compare"]
+               shared: ["lists_repeat", "lists_getIndex", "lists_setIndex", "controls_repeat_ext", "controls_if", "math_number", "logic_compare", "logic_boolean"],
+               easy: [],
+               medium: ["math_arithmetic"],
+               hard: ["lists_create_with_empty", "math_arithmetic"]
             }
          }
       },
