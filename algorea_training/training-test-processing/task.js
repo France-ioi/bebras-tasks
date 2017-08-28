2d281fbf31b244427d7bab8e69fef1110642232e
Sylvain Chiron <chironsylvain@orange.fr>
2017-08-28 17:53:33 +0200

    Update training-test-processing
    Signed-off-by: Sylvain Chiron <chironsylvain@orange.fr>

--- algorea_training/training-test-processing/task.js
+++ algorea_training/training-test-processing/task.js
@@ -7,23 +7,23 @@ function initTask(subTask) {
       includeBlocks: {
          groupByCategory: true,
          generatedBlocks: {
-            processing: ["popStyle", "pushStyle", "cursor", "focused", "frameCount", "frameRate", "__frameRate", "width", "height", "resize", "arc", "ellipse", "line", "point", "quad", "rect", "triangle", "bezier", "bezierDetail", "bezierPoint", "bezierTangent", "curve", "curveDetail", "curvePoint", "curveTangent", "curveTightness", "box", "sphere", "sphereDetail", "ellipseMode", "noSmooth", "rectMode", "smooth", "strokeCap", "strokeJoin", "strokeWeight", "beginShape", "bezierVertex", "curveVertex", "endShape", "texture", "textureMode", "vertex", "shape", "shapeMode", "isVisible", "setVisible", "disableStyle", "enableStyle", "getChild", "print", "println", "applyMatrix", "popMatrix", "printMatrix", "pushMatrix", "resetMatrix", "rotate", "rotateX", "rotateY", "rotateZ", "scale", "translate", "ambientLight", "directionalLight", "lightFalloff", "lightSpecular", "lights", "noLights", "normal", "pointLight", "spotLight", "beginCamera", "camera", "endCamera", "frustum", "ortho", "perspective", "printCamera", "printProjection", "modelX", "modelY", "modelZ", "screenX", "screenY", "screenZ", "ambient", "emissive", "shininess", "specular", "background", "colorMode", "fill", "noFill", "noStroke", "stroke", "alpha", "blendColor", "blue", "brightness", "color", "green", "hue", "lerpColor", "red", "saturation", "createImage", "image", "imageMode", "noTint", "tint", "blend", "copy", "filter", "get", "loadPixels", "pixels", "set", "updatePixels", "createGraphics", "createFont", "loadFont", "text", "textFont", "textAlign", "textLeading", "textMode", "textSize", "textWidth", "textAscent", "textDescent"]
+            processing: ["popStyle", "pushStyle", "cursor", "focused", "frameCount", "frameRate", "__frameRate", "width", "height", "resize", "arc", "ellipse", "line", "point", "quad", "rect", "triangle", "bezier", "bezierDetail", "bezierPoint", "bezierTangent", "curve", "curveDetail", "curvePoint", "curveTangent", "curveTightness", "box", "sphere", "sphereDetail", "ellipseMode", "noSmooth", "rectMode", "smooth", "strokeCap", "strokeJoin", "strokeWeight", "beginShape", "bezierVertex", "curveVertex", "endShape", "texture", "textureMode", "vertex", "shape", "shapeMode", "isVisible", "setVisible", "disableStyle", "enableStyle", "getChild", "print", "println", "applyMatrix", "popMatrix", "printMatrix", "pushMatrix", "resetMatrix", "rotate", "rotateX", "rotateY", "rotateZ", "scale", "translate", "ambientLight", "directionalLight", "lightFalloff", "lightSpecular", "lights", "noLights", "normal", "pointLight", "spotLight", "beginCamera", "camera", "endCamera", "frustum", "ortho", "perspective", "printCamera", "printProjection", "modelX", "modelY", "modelZ", "screenX", "screenY", "screenZ", "ambient", "emissive", "shininess", "specular", "background", "colorMode", "fill", "noFill", "noStroke", "stroke", "alpha", "blendColor", "blue", "brightness", "color", "green", "hue", "lerpColor", "red", "saturation", "createImage", "image", "imageMode", "noTint", "tint", "blend", "copy", "filter", "get", "loadPixels", "pixels", "set", "updatePixels", "createGraphics", "createFont", "loadFont", "text_", "textFont", "textAlign", "textLeading", "textMode", "textSize", "textWidth", "textAscent", "textDescent"]
          },
          standardBlocks: {
-            includeAll: true
+            wholeCategories: ["input", "logic", "loops", "math", "texts", "lists", "colour", "dicts", "variables", "functions"]
          }
       },
       maxInstructions: 100,
       checkEndEveryTurn: false,
       checkEndCondition: function(context, lastTurn) {
          var ops = [
-            { func: 'fill', args: [0, 0, 0] },
+            { func: 'fill', args: [0] },
             { func: 'ellipse', args: [150, 180, 180, 180] },
             { func: 'ellipse', args: [70, 70, 100, 100] },
             { func: 'ellipse', args: [230, 70, 100, 100] }
          ];
          if (context.processing.ops.length != ops.length) {
-            throw(strings.drawingWrong);
+            throw(context.processing.ops.length > ops.length ? strings.drawingIndirect : strings.drawingWrong);
          }
          for (var iOp = 0; iOp < context.processing.ops.length && iOp < ops.length; iOp++) {
             var cOp = context.processing.ops[iOp];
