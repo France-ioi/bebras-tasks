31109541ce845c9a30702379d73d3fc27cfd1d17
Sebastien Carlier <s.carlier@epixode.fr>
2018-01-09 15:35:48 +0000

    hoist nested function

--- alkindi_2016_2017/2017-FR-AL-01-reused-key/src/algorea_react_task/legacy/task.js
+++ alkindi_2016_2017/2017-FR-AL-01-reused-key/src/algorea_react_task/legacy/task.js
@@ -1,57 +1,69 @@
+
 export default function () {
 
     return {
-
-        showViews: function (_views, success, _error) {
-            success();
-        },
-
-        getViews: function (success, _error) {
-            success({});
-        },
-
-        updateToken: function (_token, success, _error) {
-            success();
-        },
-
-        getHeight: function (success, _error) {
-            var d = document;
-            var h = Math.max(d.body.offsetHeight, d.documentElement.offsetHeight);
-            success(h);
-        },
-
-        unload: function (success, _error) {
-            success();
-        },
-
-        getState: function (success, _error) {
-            success('');
-        },
-
-        getMetaData: function (success, _error) {
-            success({});
-        },
-
-        reloadAnswer: function (_answer, success, _error) {
-            success();
-        },
-
-        reloadState: function (_state, success, _error) {
-            success();
-        },
-
-        getAnswer: function (success, _error) {
-            success('');
-        },
-
-        load: function (_views, success, _error) {
-            success();
-        },
-
-        gradeAnswer: function (_answer, _answerToken, success, _error) {
-            success(0, '');
-        }
-
+        showViews,
+        getViews,
+        updateToken,
+        getHeight,
+        unload,
+        getState,
+        getMetaData,
+        reloadAnswer,
+        reloadState,
+        getAnswer,
+        load,
+        gradeAnswer,
     };
 
+}
+
+function showViews (_views, success, _error) {
+    success();
+}
+
+function getViews (success, _error) {
+    success({});
+}
+
+function updateToken (_token, success, _error) {
+    success();
+}
+
+function getHeight (success, _error) {
+    var d = document;
+    var h = Math.max(d.body.offsetHeight, d.documentElement.offsetHeight);
+    success(h);
+}
+
+function unload (success, _error) {
+    success();
+}
+
+function getState (success, _error) {
+    success('');
+}
+
+function getMetaData (success, _error) {
+    success({});
+}
+
+function reloadAnswer (_answer, success, _error) {
+    success();
+}
+
+function reloadState (_state, success, _error) {
+    success();
+}
+
+function getAnswer (success, _error) {
+    success('');
+}
+
+function load (_views, success, _error) {
+    success();
+}
+
+function gradeAnswer (_answer, _answerToken, success, _error) {
+    success(0, '');
 }
