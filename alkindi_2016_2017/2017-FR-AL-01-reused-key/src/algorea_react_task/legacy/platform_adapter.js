f41050bf5119e3659c4b55ff5d002ae6c389ff89
Sebastien Carlier <s.carlier@epixode.fr>
2018-01-09 16:02:52 +0000

    add getTaskParams to platformAdapter

--- alkindi_2016_2017/2017-FR-AL-01-reused-key/src/algorea_react_task/legacy/platform_adapter.js
+++ alkindi_2016_2017/2017-FR-AL-01-reused-key/src/algorea_react_task/legacy/platform_adapter.js
@@ -7,12 +7,18 @@ export default function (platform) {
         });
     }
 
+    function getTaskParams (key, defaultValue) {
+        return new Promise(function (resolve, reject) {
+            platform.getTaskParams(key, defaultValue, resolve, reject);
+        });
+    }
+
     function validate (mode) {
         return new Promise(function (resolve, reject) {
             platform.validate(mode, resolve, reject);
         });
     }
 
-    return {initWithTask, validate};
+    return {initWithTask, getTaskParams, validate};
 
 }
