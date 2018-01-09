c3e44f1c6f9cd391ddf3539fa221c5f066d659bd
Sebastien Carlier <s.carlier@epixode.fr>
2018-01-09 14:56:43 +0000

    emitter has no interesting return value

--- alkindi_2016_2017/2017-FR-AL-01-reused-key/src/algorea_react_task/platform_channel.js
+++ alkindi_2016_2017/2017-FR-AL-01-reused-key/src/algorea_react_task/platform_channel.js
@@ -6,30 +6,28 @@ export default (task) => {
     return eventChannel(emitter => {
 
         task.load = function(views, callback) {
-            return emitter({type: 'load', views, callback});
+            emitter({type: 'load', views, callback});
         };
 
         task.getAnswer = function(callback) {
-            return emitter({type: 'getAnswer', callback});
+            emitter({type: 'getAnswer', callback});
         };
 
         task.reloadAnswer = function(answer, callback) {
-            return emitter({type: 'reloadAnswer', answer, callback});
+            emitter({type: 'reloadAnswer', answer, callback});
         };
 
         task.gradeAnswer = function(answer, answer_token, callback) {
-
-            return emitter({type: 'gradeAnswer', answer, answer_token, callback});
+            emitter({type: 'gradeAnswer', answer, answer_token, callback});
         };
 
 
         task.getState = function(callback) {
-            return emitter({type: 'getState', callback});
+            emitter({type: 'getState', callback});
         };
 
         task.reloadState = function(state, callback) {
-
-            return emitter({type: 'reloadState', state, callback});
+            emitter({type: 'reloadState', state, callback});
         };
 
         return () => {};
