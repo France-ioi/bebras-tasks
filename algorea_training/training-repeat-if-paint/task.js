a372a6631af06e415b907ba924555e2237daf21c
mathias-hiron <mathias.hiron@gmail.com>
2018-01-12 22:11:05 +0100

    fix number of blocks

--- algorea_training/training-repeat-if-paint/task.js
+++ algorea_training/training-repeat-if-paint/task.js
@@ -10,7 +10,7 @@ function initTask(subTask) {
          paint: { img: "paint.png", side: cellSide, category: "paint", isPaint: true, isObstacle: false, hasColor: true, color: "gris", zOrder: 1 },
          marker: { num: 2, img: "marker.png", side: cellSide, category: "marker", isObstacle: false, isMarker: true, zOrder: 0 }
       },
-      maxInstructions: 10,
+      maxInstructions: {easy: 10, medium: 10, hard: 18},
       includeBlocks: {
          groupByCategory: false,
          generatedBlocks: {
