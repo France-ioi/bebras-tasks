dfd6038d9ee2592416282fca94cca44a129d545f
mobydimk <mobydimk@gmail.com>
2018-02-05 18:31:51 +0400

    map task

---
+++ module_testing/test-map/task.js
@@ -0,0 +1,46 @@
+function initTask(subTask) {
+
+    subTask.gridInfos = {
+        hideSaveOrLoad: false,
+        actionDelay: 200,
+        buttonScaleDrawing: false,
+
+        includeBlocks: {
+            groupByCategory: false,
+            generatedBlocks: {
+                map: [
+                    'clearMap',
+                    'addLocation',
+                    'addRoad',
+                    'geoDistance',
+                    'getLatitude',
+                    'getLongitude',
+                    'getNeighbors',
+                    'shortestPath'
+                ]
+            },
+            standardBlocks: {
+                includeAll: false
+            }
+        },
+        maxInstructions: 100,
+        checkEndEveryTurn: false,
+        checkEndCondition: function(context, lastTurn) {
+            context.success = true;
+            throw(strings.complete);
+        },
+        mapConfig: {
+            pin_file: 'img/pin.png',
+            map_file: 'img/carteDeFrance.png',
+            map_lng_left: -4.85,
+            map_lng_right: 9.65,
+            map_lat_bottom: 41.7
+        }
+    }
+
+    subTask.data = {
+        easy: [{}]
+    }
+    initBlocklySubTask(subTask)
+}
+initWrapper(initTask, null, null)
