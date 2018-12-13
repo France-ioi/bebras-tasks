d1fa67f0fb9a3384e3c27b4f6a8438ddc43be7e9
UncleBone <arnaud.lgrd@gmail.com>
2018-12-13 10:39:08 +0100

    2014-SI-02-alien-language: upgrade to new format + easy & hard

--- 2014/2014-SI-02-alien-language/task.js
+++ 2014/2014-SI-02-alien-language/task.js
@@ -1,18 +1,107 @@
-function initTask() {
-   var instructions = [
-      "Mi", 
-      "La",   
-      "Si",    
-      "Do"
-      ];
-   var prefix = [0, 0, 1, 1, 2 ];
-   var maxSequenceLength = 7;
+function initTask(subTask) {
+   var state = {};
+   var level;
+   var answer = null;
+   var data = {
+      easy: {
+         instructions: [ "Mi", "La", "Si", "Do" ],
+         prefix: [0, 2, 2, 1, 3 ],
+         maxSequenceLength: 5,
+         solution: [ [ 1, 2, 1, 3, 3 ] ]
+      },
+      medium: {
+         instructions: [ "Mi", "La", "Si", "Do" ],
+         prefix: [0, 0, 1, 1, 2 ],
+         maxSequenceLength: 7,
+         solution: [
+            [ 3, 2, 2, 1, 0, 1, 2 ],
+            [ 1, 0, 1, 2, 2, 3, 2 ] ]
+      },
+      hard: {
+         instructions: [ "Mi", "La", "Si" ],
+         prefix: [1, 1, 2, 1, 1, 2, 1, 2 ],
+         maxSequenceLength: 12,
+         solution: [ [ 1, 0, 1, 1, 0, 1, 0, 1, 2, 1, 2, 1 ] ]
+      }
+   };
+   var instructions;
+   var prefix;
+   var maxSequenceLength;
+   var solution;
    var dragAndDrop;
    var instructionDefs;
    var widthLabel = 40;
    var heightLabel = 35;
+   var spaceLabel = 40;
    var paper;
+   var paperWidth = 770;
+   var paperHeight = 130;
 
+   subTask.loadLevel = function(curLevel) {
+      level = curLevel;
+      prefix = data[level].prefix;
+      instructions = data[level].instructions;
+      maxSequenceLength = data[level].maxSequenceLength;
+      solution = data[level].solution;
+      paperHeight += (level == "hard") ? (widthLabel + spaceLabel) : 0;
+   };
+
+   subTask.getStateObject = function() {
+      return state;
+   };
+
+   subTask.reloadAnswerObject = function(answerObj) {
+      answer = answerObj;
+      if(answer){
+
+      }
+   };
+
+   subTask.resetDisplay = function() {
+      drawPaper();
+      initPrefix();
+   };
+
+   subTask.getAnswerObject = function() {
+      return answer;
+   };
+
+   subTask.getDefaultAnswerObject = function() {
+      var defaultAnswer = [];
+      return defaultAnswer;
+   };
+
+   subTask.unloadLevel = function(callback) {
+      callback();
+   };
+
+   function getResultAndMessage() {
+      var result;
+      if (answer.length == 0) {
+            result = { successRate: 0, message: taskStrings.placeNotesOnCells};
+         } else if (isCorrect(answer)) {
+            result = { successRate: 1, message: taskStrings.success };
+         } else {
+            result = { successRate: 0, message: taskStrings.failure };
+            // <br> (Le robot ne se déplace pas sur l'image, c'est normal.)
+         }
+      return result;
+   }
+
+   subTask.getGrade = function(callback) {
+      callback(getResultAndMessage());
+   };
+
+   function initPrefix() {
+      var text = "";
+      for(var iPrefix = 0; iPrefix < prefix.length; iPrefix++){
+         text += instructions[prefix[iPrefix]];
+         if(iPrefix < prefix.length - 1){
+            text += ", ";
+         }
+      }
+      $("#prefix").text(text);
+   };
 
    function stackSize() {
       var objects = dragAndDrop.getObjects('seq');
@@ -23,21 +112,10 @@ function initTask() {
       return size;
    }
 
-
-   task.load = function(views, callback) {
-      // displayHelper.hideValidateButton = true;
-      //task.solutions
-      drawPaper();
-      if (views.solution) {
-         $.each(task.solutions, function(idSolution, solution) {
-            var labels = $.map(solution, function(iInstr) {return instructions[iInstr]; });
-            $("#textSolution" + idSolution).html(labels.join(", "));
-         });
-      }
-      callback();
-   }
-   
    var makeInstr = function(instr, x, y) {
+      if(!instr){
+         return [];
+      }
       var label = paper.rect(x-widthLabel/2, y-heightLabel/2, widthLabel, heightLabel, heightLabel/5)
                       .attr({'fill': '#E0E0F8'});
       var text = paper.text(x, y, instr)
@@ -55,15 +133,15 @@ function initTask() {
    };
 
    var drawPaper = function() {
-      var width = 512;
-      var height = 130;
+      var width = paperWidth;
+      var height = paperHeight;
       var sequenceY = 100;
-
-      paper = Raphael("programming", width, height);
+      paper = subTask.raphaelFactory.create("programming","programming",width,height);
       dragAndDrop = DragAndDropSystem({
          paper : paper,
          drop : function(srcContId, srcPos, dstContId, dstPos, type) {
             displayHelper.stopShowingResult();
+            answer = dragAndDrop.getObjects('seq');
          },
          actionIfDropped : function(srcCont, srcPos, dstCont, dstPos, dropType)
          {
@@ -79,16 +157,28 @@ function initTask() {
             return dstCont == null;
          }
       });
+      var prefixW = prefix.length * widthLabel;
+      var containerW = maxSequenceLength * widthLabel;
+      if(level != "hard"){
+         var spacing = 10;
+         var prefixX = (paperWidth - (prefixW + containerW + spacing))/2;
+         var containerY = sequenceY;
+         var containerX = spacing + prefixX + prefixW + (widthLabel*maxSequenceLength)/2;
+      }else{
+         var prefixX = (paperWidth - prefixW)/2 + widthLabel/2;
+         var containerY = sequenceY + widthLabel + spaceLabel;
+         var containerX = (paperWidth - containerW)/2 + (widthLabel*maxSequenceLength)/2;
+      }
       for (var iPrefix = 0; iPrefix < prefix.length; iPrefix++) {
          var str = instructions[prefix[iPrefix]];
-         var elems = makeInstr(str, 30 + iPrefix * widthLabel, sequenceY);
+         var elems = makeInstr(str, prefixX + iPrefix * widthLabel, sequenceY);
       }
       var backgroundTarget = paper.rect(-widthLabel/2,-heightLabel/2,widthLabel,heightLabel)
          .attr('fill', '#F2F2FF');
       dragAndDrop.addContainer({
          ident : 'seq',
-         cx : 30 + prefix.length * widthLabel + (widthLabel*maxSequenceLength)/2,
-         cy : sequenceY,
+         cx : containerX,   // container pos
+         cy : containerY,
          widthPlace : widthLabel, 
          heightPlace : heightLabel,
          nbPlaces : maxSequenceLength,
@@ -99,12 +189,13 @@ function initTask() {
          placeBackgroundArray : [backgroundTarget]
       });
       instructionDefs = [];
+      var instructionsW = instructions.length*widthLabel + (instructions.length - 1)*spaceLabel;
+      var instructionsX = (paperWidth - instructionsW)/2;
       for (var iInstr = 0; iInstr < instructions.length; iInstr++) {
          var elems = makeInstr(instructions[iInstr], 0, 0);
-         var spaceLabel = 40;
          instructionDefs[iInstr] = dragAndDrop.addContainer({
             ident : iInstr,
-            cx : 120 + widthLabel/2 + iInstr * (widthLabel+spaceLabel), 
+            cx : instructionsX + widthLabel/2 + iInstr * (widthLabel+spaceLabel),      // source pos
             cy : 10 + heightLabel/2, 
             widthPlace : widthLabel, 
             heightPlace : heightLabel,
@@ -113,57 +204,22 @@ function initTask() {
             align: 'left'
          });
       }
-   };
-
-   var answerOfStrAnswer = function(strAnswer) {
-      if (strAnswer == "") {
-         return [];
-      }
-      return $.parseJSON(strAnswer);
-   };
-
-   task.reloadAnswer = function(strAnswer, callback) {
-      var sequence = answerOfStrAnswer(strAnswer);
-
-      // code avec les nouvelles fonctions :
-      dragAndDrop.removeAllObjects('seq');
-      dragAndDrop.insertObjects('seq', 0, $.map(sequence, function(iInstr) {
+      dragAndDrop.insertObjects('seq', 0, $.map(answer, function(iInstr) {
          return {
             ident : iInstr,
             elements: makeInstr(instructions[iInstr], 0, 0)
          };
       }));
-      callback();
    };
 
-   task.getAnswer = function(callback) {
-      var seq = dragAndDrop.getObjects('seq');
-      var sequence = [];
-      var iObject = 0;
-      while ((iObject < seq.length) && (seq[iObject] != null)) {
-         sequence.push(seq[iObject]);
-         iObject++;
-      }
-      callback(JSON.stringify(sequence));
-   };
-
-   task.isCorrect = function(sequence) {
-      return Beav.Object.eq(sequence, task.solutions[0]) 
-          || Beav.Object.eq(sequence, task.solutions[1]);
-   };
-
-   grader.gradeTask = function(strAnswer, token, callback) {
-      platform.getTaskParams(null, null, function(taskParams) {
-         var sequence = answerOfStrAnswer(strAnswer);
-         if (sequence.length == 0) {
-            callback(taskParams.noScore, taskStrings.placeNotesOnCells);
-         } else if (task.isCorrect(sequence)) {
-            callback(taskParams.maxScore, taskStrings.success);
-         } else {
-            callback(taskParams.minScore, taskStrings.failure);
-            // <br> (Le robot ne se déplace pas sur l'image, c'est normal.)
+   var isCorrect = function(sequence) {
+      for(var iSol = 0; iSol < solution.length; iSol++){
+         if(Beav.Object.eq(sequence, solution[iSol])){
+            return true;
          }
-      });
+      }
+      return false;
    };
 }
-initTask();
+initWrapper(initTask, ["easy", "medium", "hard"]);
+displayHelper.useFullWidth();
