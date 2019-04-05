03816257b0d3652dbcc02eddc703ab456ee12bfb
UncleBone <arnaud.lgrd@gmail.com>
2019-04-05 10:21:25 +0200

    test-automata: turned into a subject template

--- examples/module_testing/test-automata/task.js
+++ examples/module_testing/test-automata/task.js
@@ -2,16 +2,82 @@ function initTask(subTask) {
    var state = {};
    var level;
    var answer = null;
+   var data = {
+      easy: {
+         regex: '(A|B)C',
+         vGraph: {
+            "vertexVisualInfo":{
+               "v_0":{"x":224,"y":96},
+               "v_1":{"x":320,"y":96},
+               "v_2":{"x":416,"y":96},
+               "v_3":{"x":512,"y":96}},
+            "edgeVisualInfo":{
+               "e_0":{},"e_1":{},"e_2":{}},
+            "minGraph":{
+               "vertexInfo":{"v_0":{"terminal":true},"v_1":{},"v_2":{},"v_3":{"terminal":true}},
+               "edgeInfo":{"e_0":{"label":"A"},"e_1":{"label":"B"},"e_2":{"label":"C"}},
+               "edgeVertices":{"e_0":["v_0","v_1"],"e_1":["v_1","v_2"],"e_2":["v_2","v_3"]},
+            "directed":true}
+         },
+         transitionTable: {
+            "s0":{"A":["s1"],"B":["s1"]},
+            "s1":{"C":["s2"]},
+            "s2":{}
+         },
+         start: "s0",
+         end: "s2",
+         paperHeight: 200
+      },
+      medium: {
+         regex: 'A+B[C-E]{3}',
+         vGraph: {
+            "vertexVisualInfo":{"v_0":{"x":64,"y":128},"v_1":{"x":192,"y":128},"v_2":{"x":320,"y":128},"v_3":{"x":448,"y":128},"v_4":{"x":576,"y":128},"v_5":{"x":704,"y":128},"v_6":{"x":320,"y":224},"v_7":{"x":448,"y":224}},
+            "edgeVisualInfo":{"e_0":{},"e_1":{},"e_2":{},"e_3":{},"e_4":{},"e_5":{},"e_6":{},"e_7":{}},
+            "minGraph":{"vertexInfo":{"v_0":{"terminal":true},"v_1":{},"v_2":{},"v_3":{},"v_4":{},"v_5":{"terminal":true},"v_6":{},"v_7":{}},
+            "edgeInfo":{"e_0":{"label":"A"},"e_1":{"label":"B"},"e_2":{"label":"C"},"e_3":{"label":"D"},"e_4":{"label":"E"},"e_5":{"label":"F"},"e_6":{"label":"G"},"e_7":{"label":"H"}},
+            "edgeVertices":{"e_0":["v_0","v_1"],"e_1":["v_1","v_2"],"e_2":["v_2","v_3"],"e_3":["v_3","v_4"],"e_4":["v_4","v_5"],"e_5":["v_1","v_6"],"e_6":["v_6","v_7"],"e_7":["v_7","v_4"]},
+            "directed":true}
+         },
+         transitionTable: {
+            "s0":{"A":["s0","s1"]},
+            "s1":{"B":["s2"]},
+            "s2":{"C":["s3"],"D":["s3"],"E":["s3"]},
+            "s3":{"C":["s4"],"D":["s4"],"E":["s4"]},
+            "s4":{"C":["s5"],"D":["s5"],"E":["s5"]},
+            "s5":{}
+         },
+         start: "s0",
+         end: "s5",
+         paperHeight: 350
+      },
+      hard: {
+         regex: 'A(B?|FG)[CH][DE]*',
+         vGraph: {
+            "vertexVisualInfo":{"v_0":{"x":64,"y":128},"v_1":{"x":192,"y":128},"v_2":{"x":320,"y":128},"v_3":{"x":448,"y":128},"v_4":{"x":576,"y":128},"v_5":{"x":704,"y":128},"v_6":{"x":320,"y":224},"v_7":{"x":448,"y":224}},
+            "edgeVisualInfo":{"e_0":{},"e_1":{},"e_2":{},"e_3":{},"e_4":{},"e_5":{},"e_6":{},"e_7":{}},
+            "minGraph":{"vertexInfo":{"v_0":{"terminal":true},"v_1":{},"v_2":{},"v_3":{},"v_4":{},"v_5":{"terminal":true},"v_6":{},"v_7":{}},
+            "edgeInfo":{"e_0":{"label":"A"},"e_1":{"label":"B"},"e_2":{"label":"C"},"e_3":{"label":"D"},"e_4":{"label":"E"},"e_5":{"label":"F"},"e_6":{"label":"G"},"e_7":{"label":"H"}},
+            "edgeVertices":{"e_0":["v_0","v_1"],"e_1":["v_1","v_2"],"e_2":["v_2","v_3"],"e_3":["v_3","v_4"],"e_4":["v_4","v_5"],"e_5":["v_1","v_6"],"e_6":["v_6","v_7"],"e_7":["v_7","v_4"]},
+            "directed":true}
+         },
+         transitionTable: {
+            "s0":{"A":["s1"]},
+            "s1":{"B":["s3"],"F":["s2"],"":["s3"]},
+            "s2":{"G":["s3"]},
+            "s3":{"C":["s4"],"H":["s4"]},
+            "s4":{"D":["s4"],"E":["s4"],"":["s5"]},
+            "s5":{}
+         },
+         start: "s0",
+         end: "s5",
+         paperHeight: 350
+      }
+   };
 
-   var defaultVisualGraph = {
-      "vertexVisualInfo":{"v_0":{"x":64,"y":128},"v_1":{"x":192,"y":128},"v_2":{"x":320,"y":128},"v_3":{"x":448,"y":128},"v_4":{"x":576,"y":128},"v_5":{"x":704,"y":128},"v_6":{"x":320,"y":224},"v_7":{"x":448,"y":224}},
-      "edgeVisualInfo":{"e_0":{},"e_1":{},"e_2":{},"e_3":{},"e_4":{},"e_5":{},"e_6":{},"e_7":{}},
-      "minGraph":{"vertexInfo":{"v_0":{"terminal":true},"v_1":{},"v_2":{},"v_3":{},"v_4":{},"v_5":{"terminal":true},"v_6":{},"v_7":{}},
-      "edgeInfo":{"e_0":{"label":"E"},"e_1":{"label":"B"},"e_2":{"label":"C"},"e_3":{"label":"D"},"e_4":{"label":"E"},"e_5":{"label":"F"},"e_6":{"label":"G"},"e_7":{"label":"H"}},
-      "edgeVertices":{"e_0":["v_0","v_1"],"e_1":["v_1","v_2"],"e_2":["v_2","v_3"],"e_3":["v_3","v_4"],"e_4":["v_4","v_5"],"e_5":["v_1","v_6"],"e_6":["v_6","v_7"],"e_7":["v_7","v_4"]},
-      "directed":true}};
-   var startID = "v_0";
-   var endID = "v_5";
+   var vGraph;
+   var regex;
+   // var startID = "v_0";
+   // var endID = "v_5";
 
    var defaultCircleAttr = {
       "r": 15, 
@@ -28,11 +94,11 @@ function initTask(subTask) {
       "font-family": "monospace",
       "font-size": 20
    };
-   var sequence = ["E","M","I","L","E"];
+   // var sequence = ["E","M","I","L","E"];
    var alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
    var paperWidth = 750;
-   var paperHeight = 350;
+   var paperHeight;
    var margin = 10;
 
    var graphPaper;
@@ -44,6 +110,7 @@ function initTask(subTask) {
    var automata;
    // var graphEditor;
    // var snapToGrid;
+   var targetNFA;
 
    var selectedCircleAttr = {
       stroke: "blue",
@@ -64,6 +131,10 @@ function initTask(subTask) {
    subTask.loadLevel = function(curLevel) {
       level = curLevel;
       displayHelper.customValidate = validation;
+      vGraph = JSON.parse(JSON.stringify(data[level].vGraph));
+      regex = data[level].regex;
+      targetNFA = new NFA(alphabet,data[level].transitionTable,[data[level].start],[data[level].end]);
+      paperHeight = data[level].paperHeight;
    };
 
    subTask.getStateObject = function() {
@@ -72,11 +143,14 @@ function initTask(subTask) {
 
    subTask.reloadAnswerObject = function(answerObj) {
       answer = answerObj;
+      if(answer)
+         vGraph = JSON.parse(answer);
    };
 
    subTask.resetDisplay = function() {
+      $("#regex").text(regex);
       initPaper();
-      initAlphabet();
+      // initAlphabet();
       initAutomata();
       initHandlers();
       updateAutomata();
@@ -88,7 +162,7 @@ function initTask(subTask) {
    };
 
    subTask.getDefaultAnswerObject = function() {
-      var defaultAnswer = [];
+      var defaultAnswer = null;
       return defaultAnswer;
    };
 
@@ -102,12 +176,13 @@ function initTask(subTask) {
 
    subTask.getGrade = function(callback) {
       callback({
-         successRate: 1
+         successRate: 1, message: taskStrings.success
       });
    };
 
    function initPaper() {
       graphPaper = subTask.raphaelFactory.create("graph", "graph", paperWidth, paperHeight);
+      graphPaper.rect(0,0,paperWidth,paperHeight);
       sequencePaper = subTask.raphaelFactory.create("sequence","sequence",paperWidth/2,50);
    };
 
@@ -125,16 +200,18 @@ function initTask(subTask) {
          subTask: subTask,
          graphPaper: graphPaper,
          graphPaperElementID: "graph",
-         visualGraphJSON: JSON.stringify(defaultVisualGraph),
-         startID: startID,
-         endID: endID,
+         visualGraphJSON: JSON.stringify(vGraph),
+         // startID: startID,
+         // endID: endID,
          circleAttr: defaultCircleAttr,
          edgeAttr: defaultLineAttr,
          sequencePaper: sequencePaper,
          sequence: sequence,
          seqLettersAttr: seqLettersAttr,
          resetCallback: resetCallback,
+         callback: saveAnswer,
          alphabet: alphabet,
+         targetNFA: targetNFA,
          enabled: true
       };
       automata = new Automata(settings);
@@ -195,13 +272,30 @@ function initTask(subTask) {
 
    function validation() {
       automata.resetAnimation();
-      automata.try(handleResult);
+      // automata.try(handleResult);
       var res = automata.compareWithTarget();
-      var html = "Automata equivalent to target: "+res.equivalent+"<br>";
-      html += "Unused vertices: "+res.unusedVertices+"<br>";
+      if(res.error){
+         $("#feedback").text(res.error);
+         return;
+      }
+      // var html = "Automata equivalent to target: "+res.equivalent+"<br>";
+      // html += "Unused vertices: "+res.unusedVertices+"<br>";
       // console.log(res.minNFA);
       // console.log(res.minTarget);
-      $("#result").html(html);
+      // $("#result").html(html);
+      if(res.equivalent){
+         displayHelper.validate("stay");
+      }else{
+         if(res["e_c"][0]){
+            automata.setSequence(res["e_c"][0]);
+            var text = "The following string is accepted by the automata but doesn't match the regex:<br>"+res["e_c"][0];
+         }else{
+            automata.setSequence(res["e_c"][1]);
+            var text = "The following string is not accepted by the automata but matches the regex:<br>"+res["e_c"][1];
+         }
+         automata.run();
+         $("#feedback").html(text);
+      }
    };
 
    function handleResult(result) {
@@ -215,5 +309,10 @@ function initTask(subTask) {
       $("#result").empty();
       $("#feedback").empty();
    };
+
+   function saveAnswer() {
+      answer = automata.visualGraph.toJSON();
+   };
 }
 initWrapper(initTask, ["easy", "medium", "hard"]);
+displayHelper.useFullWidth();
